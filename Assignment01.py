ebdb6e6415c013cc40534e27ffbc86bd71b4d73e
CHINMOY PARAMANICK <cparamanick826@gmail.com>
2025-05-17 11:03:40 +0530

    Add files via upload

---
+++ Assignment01.py
@@ -0,0 +1,17 @@
+#ASSIGNMENT 1:
+#Task 1:
+firstn=int(input("Enter the first number : "))
+secn=int(input("Enter the seconed number : "))
+addi=firstn+secn
+subst=firstn-secn
+multi=firstn*secn
+divi=firstn/secn
+print("Addition : ", addi)
+print("Substraction : ", subst)
+print("Multiplication : ", multi)
+print("Division : ", divi)
+
+#Task 2:
+firstname=input("Enter your first name : ")
+lastname=input("Enter your last name : ")
+print(f"Hello, {firstname+lastname}! welcome to the python programme")
